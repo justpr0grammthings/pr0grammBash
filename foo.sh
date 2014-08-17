c20ee6895c61df9bcc8afebc854ffbac5aec9f10
Sascha M <sascha.meissner@docinsider.de>
2014-08-17 13:30:30 +0200

    init

---
+++ foo.sh
@@ -0,0 +1,717 @@
+
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack="\033[1;90m"      # Black
+BIRed="\033[1;91m"        # Red
+BIGreen="\033[1;92m"      # Green
+BIYellow="\033[1;93m"     # Yellow
+BIBlue="\033[1;94m"       # Blue
+BIPurple="\033[1;95m"     # Purple
+BICyan="\033[1;96m"       # Cyan
+BIWhite="\033[1;97m"      # White
+
+open_red_escape="\033[31m"
+open_blue_escape="\033[34m"
+open_green_escape="\033[32m"
+open_foo_escape="\033[4m"
+close_escape="^\033[0m"
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+close_escape="^\033[0m"
+
+
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}.d8888b.                                                  "
+echo -e "${open_red_escape}   d88P"
+echo -e "${On_Red}                 ${Color_Off}${Yellow}      8"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888  d88b. 88888b"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green} '88b888 '"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}   ${Color_Off}${Yellow}  888  88  888  "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}       ${Color_Off}${open_red_escape}  8 x "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}       ${Color_Off}${open_green_escape} 88P' 888    888  888888  "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}      ${Color_Off}${open_red_escape}  888"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                      "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       '"
+sleep 0.38
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}.d8888b.                                                  "
+echo -e "${open_red_escape}   d88P"
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888  d88b. 88888b"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b '888 '88b888 '"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}   ${Color_Off}${Yellow}  888  88  888  8xx8x8xxx888x  "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}       ${Color_Off}${open_red_escape}  888xxx d88P888  888888x x "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}       ${Color_Off}${open_green_escape}  xx8xxx8888P' 888  Y88888x88xxx88  888  888888  "
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}      ${Color_Off}${open_red_escape}  888"
+echo -e "${On_Red}${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                      "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       '"
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                       .d8888b.                                                  "
+echo -e "${open_red_escape}                 d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                                 "
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888b. 888d888888    88. "
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b88888 '888 '88b"
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${Yellow}  888  888888    888    8888  888888  888  888"
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${On_Red}          ${Color_Off}${open_red_escape}  88888  888888  888  888888  888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${open_green_escape}  8888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}            ${Color_Off}${open_red_escape}  888                           888                                         "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                      Y8b d88P                                         "
+echo -e "${On_Red}              ${Color_Off}${Green}  888                                                            "
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                       .d8888b.                                                  "
+echo -e "${open_red_escape}                 d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                                 "
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888b. 888d888888    88. "
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b88888 '888 '88b"
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${Yellow}  888  888888    888    8888  888888  888  888"
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${On_Red}          ${Color_Off}${open_red_escape}  88888  888888  888  888888  888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${open_green_escape}  8888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}            ${Color_Off}${open_red_escape}  888                           888                                         "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                      Y8b d88P                                         "
+echo -e "${On_Red}              ${Color_Off}${Green}  888                                                            "
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                                   .d8888b.                                                  "
+echo -e "${open_red_escape}                                  d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                        "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888b. 888d 888d88888b.d88b. 88888b.d88b. "
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b888P'888P'   '888 '88b888 '888 '88b"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}     ${Color_Off}${Yellow}  888  8888 "
+echo -e "${On_Red} ${On_IWhite}>>${Color_Off}${On_Red}    ${Color_Off}${open_red_escape}  888 d88P888    Y88b  d88PY88b88  88888  888"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}     ${Color_Off}${open_green_escape}  88888P' 888     'Y8888P'  '8888  8 888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}      ${Color_Off}${open_red_escape}  888                                                   "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}       Y8b d88P                                         "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       'Y88P'                                          "
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                                   .d8888b.                                                  "
+echo -e "${open_red_escape}                                  d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                                 "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}8888    888 .d88b. 888d88b.d88b. 88888b.d88b. "
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green} 888    888d88P'88b888P'  88 '88b888 '888 '88b"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${Yellow}  888  888888    88888    .d888888888  888  88"
+echo -e "${On_Red}     ${On_IWhite}>>${Color_Off}${On_Red}          ${Color_Off}${open_red_escape}  888 d88P8Y88b 888888    888  88888  888  888"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${open_green_escape}  88888P''  'Y88888888    'Y88888888  888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}            ${Color_Off}${open_red_escape}  888                           888                                         "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888              Y8b d88P                                         "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       'Y88P'                                          "
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                                   .d8888b.                                                  "
+echo -e "${open_red_escape}                                  d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                                 "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888b. 888d888888    888 .d88b. 888d888 8888b. d88b. 88888b.d88b. "
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b888P'  888    888d88P'88b888P'      '88b8 '88b888 '888 '88b"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${Yellow}  888  888888    888    8  888888    .d888888888  888  888888  88"
+echo -e "${On_Red}     ${On_IWhite}>>${Color_Off}${On_Red}          ${Color_Off}${open_red_escape}  888 d88P888    Y88b  d88PY88b 888888    888  888888  888  8888  888"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${open_green_escape}  88888P' 888     'Y8888P'  'Y88888888    'Y888888888  888   888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}            ${Color_Off}${open_red_escape}  888                           888                                         "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                  Y8b d88P                                         "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       'Y88P'                                          "
+
+
+sleep 0.38
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+echo -e
+
+echo -e "${open_blue_escape}"     
+echo -e "${BIWhite}                                   .d8888b.                                                  "
+echo -e "${open_red_escape}                                  d88P  Y88b                                                 "
+echo -e "${On_Red}                 ${Color_Off}${Yellow}                 888    888                                                 "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${open_red_escape}${On_Red}             ${Color_Off}${BIYellow}  88888b. 888d888888    888 .d88b. 888d888 8888b. 88888b.d88b. 88888b.d88b. "
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${open_green_escape}${On_Red}            ${Color_Off}${Green}  888 '88b888P'  888    888d88P'88b888P'      '88b888 '888 '88b888 '888 '88b"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${Yellow}  888  888888    888    888888  888888    .d888888888  888  888888  888  888"
+echo -e "${On_Red}     ${On_IWhite}>>${Color_Off}${On_Red}          ${Color_Off}${open_red_escape}  888 d88P888    Y88b  d88PY88b 888888    888  888888  888  888888  888  888"
+echo -e "${On_Red}    ${On_IWhite}>>${Color_Off}${On_Red}           ${Color_Off}${open_green_escape}  88888P' 888     'Y8888P'  'Y88888888    'Y888888888  888  888888  888  888"
+echo -e "${On_Red}   ${On_IWhite}>>${Color_Off}${On_Red}            ${Color_Off}${open_red_escape}  888                           888                                         "
+echo -e "${On_Red}  ${On_IWhite}>>${Color_Off}${On_Red}   ${On_IWhite}        ${Color_Off}${On_Red}  ${Color_Off}${Yellow}  888                      Y8b d88P                                         "
+echo -e "${On_Red}                 ${Color_Off}${Green}  888                       'Y88P'                                          "
+
+
+echo -e "${open_green_escape} ${close_escape}"
